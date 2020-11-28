cf1a5320ebfdf95dedd2ae56bdf42b0fe0025ae6
teneons <teneons@gmail.com>
2020-11-28 20:20:32 +0200

    made sign in

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,11 @@
 const {Router} = require('express');
 const router = Router();
 const bcryptjs = require('bcryptjs');
+const jsonwebtoken = require('jsonwebtoken');
 const {check, validationResult} = require('express-validator');
 const ModelUser = require('../models/ModelUser');
+
+const secret = 'allegedly a very secret key';
 
 //sign UP
 router.post('/signup',
@@ -13,7 +16,6 @@ router.post('/signup',
       check('password', 'Password is short').isLength({min: 8})
     ], 
     async (req, res) => {
-  
       try {
         //validation
         const err = validationResult(req)
@@ -45,9 +47,47 @@ router.post('/signup',
 })
 
 
-// //sign IN
-// router.post('/signin', async (req, res) => {
+//sign IN
+router.post('/signin',
+  [
+    check('email', 'Inputted wrong email').normalizeEmail().isEmail(),
+    check('password', 'Password is short').exists()
+  ],
+  async (req, res) => {
+    try {
+      //validation
+      const err = validationResult(req)
+      if(!err.isEmpty()) {
+        return res.status(200).json({err: err.array(), message: 'Incorrect data'})
+      }
+      
+      const {email, password} = req.body   //gets data at react
 
-// })
+      //check inputted data
+      const user = await ModelUser.findOne({email})
+      if(!user) {
+        return res.status(400).json({message: 'User not finded'})
+      }
+
+      //check - match password at DB
+      const isMatch = await bcryptjs.compare(password, user.password)
+
+      if(!isMatch) {
+        return res.status(400).json({message: 'Incorrect password'})
+      }
+
+      const token = jsonwebtoken.sign(
+        {idUser: user.id},
+        secret,
+        {expiresIn: '1h'}
+      )
+
+      res.json({token, idUser: user.id})
+
+
+    } catch (e) {
+      res.status(500).json({"message": "Something was wrong at sign up. Try again"})
+  }
+})
 
 module.exports = router;
