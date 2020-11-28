c3b61d821e4969f9e7e9878f32e86eee49db1082
teneons <teneons@gmail.com>
2020-11-28 17:36:34 +0200

    init,setup,made base project

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+  console.log(`Sever has runned on port - ${port}`)
+})
